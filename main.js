fb95e375720c004786becf34644a16797a9d5606
THitokuse <hasegawa.taichiro@gmail.com>
2019-05-05 23:44:59 +0900

    座標と

--- main.js
+++ main.js
@@ -10,6 +10,19 @@ function init() {
     renderer.setClearColor(new THREE.Color(0xEEEEEE));
     renderer.setSize(window.innerWidth, window.innerHeight);
 
+    // 2-1 平面を作成する
+    var planeGeometry = new THREE.PlaneGeometry(60, 20);
+    var planeMaterial = new THREE.MeshLambertMaterial({color: 0xffffff});
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    // 2-2 平面を傾け、位置を決める
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+
+    // 2-3 平面を追加する
+    scene.add(plane);
 }
 
 window.onload = init;
